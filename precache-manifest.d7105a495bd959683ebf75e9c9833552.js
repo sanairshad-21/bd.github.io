093246381804c8f32465d181f014324db96b25b0
hammadsaeed <hammadsaeed_1997@hotmail.com>
2020-11-14 13:20:09 +0800

    Updates

---
+++ precache-manifest.d7105a495bd959683ebf75e9c9833552.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71dcbb3c9ea9ad6fb0dacded277d26b8",
+    "url": "/bd.github.io/index.html"
+  },
+  {
+    "revision": "65412a4a4f0cf033490c",
+    "url": "/bd.github.io/static/css/main.91872e09.chunk.css"
+  },
+  {
+    "revision": "094dc2bdccf2deeb7427",
+    "url": "/bd.github.io/static/js/2.e2f82d03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bd.github.io/static/js/2.e2f82d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65412a4a4f0cf033490c",
+    "url": "/bd.github.io/static/js/main.9defb720.chunk.js"
+  },
+  {
+    "revision": "225fdffa5f74e2fe3ac9",
+    "url": "/bd.github.io/static/js/runtime-main.b5435d9a.js"
+  },
+  {
+    "revision": "8628078e0b11009e066913bd76f189ff",
+    "url": "/bd.github.io/static/media/black-font.8628078e.ttf"
+  }
+]);
